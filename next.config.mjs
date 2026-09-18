32f1722aefb5e379c43e8ac4f9d5287efaf59eca
Testacc67 <emanuelbichler13.1.2009@gmail.com>
2026-09-18 07:39:30 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // This tells Vercel to ignore linting errors during build
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // This tells Vercel to ignore TypeScript errors during build
     ignoreBuildErrors: true,
